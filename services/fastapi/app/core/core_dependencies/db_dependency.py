a494629e57ff012f821d581f2b5f80e99f2e3c6d
Andrey Frolov <darmaley40@yandex.ru>
2026-06-22 18:34:40 +0300

    refactor: introduce DBDependency class for DB management
    Создан класс DBDependency для централизованного управления подключением к БД: инициализация движка, фабрики сессий и предоставление доступа к ним. Добавлена вспомогательная функция get_db_engine. Это нужно для унификации работы с БД и упрощения внедрения зависимостей. Может повлиять на все компоненты, использующие подключение к базе — теперь они должны опираться на этот класс.

---
+++ services/fastapi/app/core/core_dependencies/db_dependency.py
@@ -0,0 +1,30 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.core.settings import settings
+
+
+class DBDependency:
+    def __init__(self) -> None:
+        self._engine = create_async_engine(
+            url=settings.db_settings.db_url, echo=settings.db_settings.db_echo
+        )
+        self._session_factory = async_sessionmaker(
+            bind=self._engine, expire_on_commit=False, autocommit=False
+        )
+
+    @property
+    def db_session(self) -> async_sessionmaker[AsyncSession]:
+        return self._session_factory
+
+    @property
+    def db_engine(self) -> AsyncEngine:
+        return self._engine
+
+
+def get_db_engine() -> AsyncEngine:
+    return DBDependency().db_engine
